168b3eaba038753120ed91d50c1598acf00c1190
Munaiz <munaizahmed2@gmail.com>
2023-11-08 11:16:20 -0600

    added backend call to API gate in about page.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,24 @@
 import Image from 'next/image'
 import styles from '../page.module.css'
 
-export default function Home() {
+async function getData(){
+
+  const res = await fetch("https://e6fzx0p7md.execute-api.us-east-1.amazonaws.com/dev")
+
+  if(!res.ok) throw new Error('Failed to fetch data')
+
+  return res.json()
+}
+
+export default async function Home() {
+  let data = await getData()
+
+  console.log('DATA =>> ', data)
+
   return (
     <main className={styles.main}>
       <h1>Hello, About page!</h1>
+
     </main>
   )
 }
