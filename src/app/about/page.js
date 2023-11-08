977ef55c5f63e4ef59b56a5cc6c9e1be32b4f726
Munaiz <munaizahmed2@gmail.com>
2023-11-07 19:57:01 -0600

    cleaned up console logs.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,11 +2,9 @@ import Image from 'next/image'
 import styles from '../page.module.css'
 
 export default function Home() {
-  console.log('hello')
   return (
     <main className={styles.main}>
       <h1>Hello, About page!</h1>
-
     </main>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  console.log('HOME')
   return (
     <main className={styles.main}>
       <h1>Hello, Home page!</h1>
